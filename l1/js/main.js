e1f145b3c8d40a71efc68aa574336ff6b677c4b0
shtorin <abelouso@gmail.com>
2020-01-30 18:11:30 +0100

    Redirect user on install or dismiss

--- l1/js/main.js
+++ l1/js/main.js
@@ -24,34 +24,38 @@ window.addEventListener('beforeinstallprompt', function (event) {
 });
 
 function installBtnClick() {
-    console.log('[Land] Button clicked');
+  console.log('[Land] Button clicked');
 
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-  
-      apiHandler.sendPromptEventRequest();
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
 
-      deferredPrompt.userChoice.then(function(choiceResult) {
-        console.log(choiceResult.outcome);
-  
-        if (choiceResult.outcome === 'dismissed') {
-          console.log('[Land] User cancelled installation');     
-        } else {  
-          apiHandler.sendInstallEventRequest()
-            .then((response) => {
-              console.log('[LandPromise] User added to home screen', response);
+    apiHandler.sendPromptEventRequest();
 
-              if (response && !response.ErrorMessage) {
-                var install = {
-				  id: response.InstallGuid, 
-				  applicationGuid: pwaConfig.AppGuid,
-                }
-                indexedDb.writeData('installs',install);
+    deferredPrompt.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('[Land] User cancelled installation');
+
+        window.location.replace('https://google.com');
+      } else {
+        apiHandler.sendInstallEventRequest()
+          .then((response) => {
+            console.log('[LandPromise] User added to home screen', response);
+
+            if (response && !response.ErrorMessage) {
+              var install = {
+                id: response.InstallGuid,
+                applicationGuid: pwaConfig.AppGuid,
               }
-            });          
-        }
-      });
-  
-      deferredPrompt = null;
-    }
+              indexedDb.writeData('installs', install);
+            }            
+          });
+
+          window.location.replace('https://google.com');
+      }
+    });
+
+    deferredPrompt = null;
+  }
 }
