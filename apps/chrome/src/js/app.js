cc4a46cd8d243aa7b0acbd2f6cd983f37288cab5
shtorin <abelouso@gmail.com>
2020-02-09 19:30:07 +0100

    Removed deprecated code from pwa app.js file

--- apps/chrome/src/js/app.js
+++ apps/chrome/src/js/app.js
@@ -1,8 +1,3 @@
-var advertisementInfo = {
-    redirectUrl: 'https://google.com',
-    advertisementId: '2E7F885F-6A8E-497C-9D74-AFDBBAF001D9',
-};
-
 function handleApplicationOpen() {
     infr.getInstallData()
         .then((install) => {
@@ -10,33 +5,15 @@ function handleApplicationOpen() {
         })
         .then(() => {
             contentManager.show();
-        }); 
+        });
 };
 
 handleApplicationOpen();
 
 if (window.matchMedia('(display-mode: standalone)').matches) {
     console.log("[PWA] This is running as standalone.");
-  }
-  else {
+}
+else {
     console.log("[Land] This is running from browser.");
-  }
+}
 
-
-function sendAdShowEventRequest() {
-    console.log('[PWA] Send AdShowEvent request', advertisementInfo);
-};
-
-function showAdvertisement() {
-    if (advertisementInfo) {
-        console.log('[PWA] Showing advertisement', advertisementInfo);
-
-        sendAdShowEventRequest();        
-    }
-};
-
-showAdvertisement();
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
