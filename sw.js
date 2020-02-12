c53e6ffb5d822cf83e4020b0d114b58cdc8615e7
shtorin <abelouso@gmail.com>
2020-02-12 19:00:28 +0100

    ms banner

--- sw.js
+++ sw.js
@@ -21,43 +21,50 @@ var STATIC_FILES = [
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-      caches.open(CACHE_STATIC_NAME)
-        .then(function (cache) {
-          console.log('[Service Worker] Precaching App Shell');
-          cache.addAll(STATIC_FILES);
-        })
+        caches.open(CACHE_STATIC_NAME)
+            .then(function (cache) {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll(STATIC_FILES);
+            })
     )
-  });
+});
 
-  self.addEventListener('activate', function (event) {
+self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ....', event);
     event.waitUntil(
-      caches.keys()
-        .then(function (keyList) {
-          return Promise.all(keyList.map(function (key) {
-            if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-              console.log('[Service Worker] Removing old cache.', key);
-              return caches.delete(key);
-            }
-          }));
-        })
+        caches.keys()
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('[Service Worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
     );
     return self.clients.claim();
-  });
+});
 
-  function isInArray(string, array) {
+function isInArray(string, array) {
     var cachePath;
-    if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
-      console.log('matched ', string);
-      cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+    if (string.indexOf(self.origin) === 0) {
+        console.log('matched ', string);
+        cachePath = string.substring(self.origin.length);
     } else {
-      cachePath = string; // store the full request (for CDNs)
+        cachePath = string;
     }
     return array.indexOf(cachePath) > -1;
-  }
-  
-  self.addEventListener('fetch', function (event) {
-      event.respondWith(
-        fetch(event.request)
-      );
-  });
+}
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    );
+});
