ba1bffa4f8047400dd7645d6f01101087060e6b5
shtorin <abelouso@gmail.com>
2020-02-19 18:29:59 +0100

    Instagram index page redirects on back button too

--- apps/instagram/src/js/app.js
+++ apps/instagram/src/js/app.js
@@ -1,4 +1,8 @@
 function handleApplicationOpen() {
+    if (window.performance && window.performance.navigation.type == window.performance.navigation.TYPE_BACK_FORWARD) {
+        window.location.replace(pwaConfig.defaultRedirect);    
+    }
+
     infr.getInstallData()
         .then((install) => {
             return apiHandler.sendOpenEventRequest(install.id);
