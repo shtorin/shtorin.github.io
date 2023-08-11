15c0e40d2b853424ad2a7f629e91bd7ed7f63414
shtorin <abelouso@gmail.com>
2023-08-11 12:19:59 +0200

    dis remote

---
+++ ms/js/cfg.js
@@ -0,0 +1,11 @@
+
+var config = {
+	apiKey: "AIzaSyDnU949ApofKmeD_P2bv_AE5bpRIdz1pkg",
+	authDomain: "shtoringithubpushes.firebaseapp.com",
+	databaseURL: "https://shtoringithubpushes.firebaseio.com",
+	projectId: "shtoringithubpushes",
+	storageBucket: "shtoringithubpushes.appspot.com",
+	messagingSenderId: "279155818136",
+	appId: "1:279155818136:web:db3e5f8199b9b2afa59aed",
+	measurementId: "G-JZHK778HK0"
+};
